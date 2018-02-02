ce5a68b9379fe7e58094a32629cd25dd57a2d501
Danny Zou <zou50@purdue.edu>
2018-02-02 16:17:23 -0500

    Update app structure and style

--- js/Game.js
+++
@@ -1,6 +0,0 @@
-var game = new Phaser.Game(500, 500, Phaser.AUTO, document.getElementById('app'), this, false, false);
-
-game.state.add('Login', Login);
-game.state.add('Menu', Menu);
-
-game.state.start('Menu');
