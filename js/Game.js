ef582856cb8ee0fe3d6a65185e01b07782a95c82
devonlee111 <devonlee111@gmail.com>
2018-02-02 15:28:29 -0500

    Added Main Menu File
    Added the file for the main menu game state.

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,6 @@
 var game = new Phaser.Game(500, 500, Phaser.AUTO, document.getElementById('app'), this, false, false);
 
 game.state.add('Login', Login);
+game.state.add('Menu', Menu);
+
+game.state.start('Menu');
