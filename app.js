a824192dd6f52537c6e2af22d4ff707f4d0338b3
Danny Zou <zou50@purdue.edu>
2018-02-02 16:09:14 -0500

    Implement data handlers

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ var io = require('socket.io').listen(server);
 app.use('/css',     express.static(__dirname + '/css'));
 app.use('/js',      express.static(__dirname + '/js'));
 app.use('/assets',  express.static(__dirname + '/assets'));
-app.use('/phaser', express.static(__dirname + '/node_modules/phaser/build'))
+app.use('/server',  express.static(__dirname + '/server'));
+app.use('/phaser',  express.static(__dirname + '/node_modules/phaser/build'))
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
---
+++ server/handler.js
@@ -0,0 +1,57 @@
+/* Primary helper functions for game-to-server handling */
+
+/*
+ *
+ * TODO: Implement local and remote databases
+ *
+ * Create a Mongo database to store data locally
+ * Send stored local Mongo database to server cloud when user is connected to network
+ *
+ */
+
+// TEMPORARY HELPER FUNCTIONS
+
+/* Save user object to local storage as JSON */
+function saveUserData(user) {
+    localStorage.setItem('user', JSON.stringify(user));
+}
+
+/* Retrieve user object */
+function getUserData() {
+    return JSON.parse(localStorage.getItem('user'));
+}
+
+/* Retrieve data from local JSON file */
+function getJSONFromFile(filename) {
+    getJSON(filename, function(response) {
+        console.log("JSON Data from %s", filename);
+        console.log(JSON.parse(response));
+    })
+}
+
+/* Helper function for getJSONFromFile() */
+function getJSON(filename, callback) {
+    var xml = new XMLHttpRequest();
+    xml.open('GET', filename, true);
+    xml.onreadystatechange = function() {
+        if (xml.readyState == 4 && xml.status == "200")
+            callback(xml.responseText);
+    }
+    xml.send(null);
+}
+
+// TEST DRIVERS
+
+getJSONFromFile("/server/users.json");
+
+
+
+
+
+
+
+
+
+
+
+
