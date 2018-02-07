bf20144d01dffab4a0073fa3b92f54016b0223ae
JamesPHawk <pjhawk208@gmail.com>
2018-02-06 21:55:31 -0500

    added buttons, linked to Menu state

--- js/Login.js
+++ js/Login.js
@@ -6,13 +6,18 @@ Login.preload = function() {
 }
 
 Login.create = function() {
+    // Create Background
     game.stage.backgroundColor = '#292c30';
     
+    // Add in sprites from spritesheet
     game.add.sprite(game.world.centerX - 350, game.world.centerY + 50, 'login');
-    button = game.add.button(game.world.centerX - 350, game.world.centerY, + 50, 'login', logIn, this);
-    
+
     game.add.sprite(game.world.centerX + 200, game.world.centerY + 50, 'signup');
-    button = game.add.button(game.world.centerX + 200, game.world.centerY + 50, 'signup', logIn, this);
+    
+    // Build buttons on top of sprites
+    loginButton = game.add.button(game.world.centerX - 350, game.world.centerY + 50, 'login', logIn, this);
+    
+    signupButton = game.add.button(game.world.centerX + 200, game.world.centerY + 50, 'signup', logIn, this);
 }
 
 function logIn() {
