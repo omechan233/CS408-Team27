964b832dc9fa0f88d14b6d541dc0004636b8bf06
devonlee111 <devonlee111@gmail.com>
2018-02-08 11:43:14 -0500

    Made Mobs Follow Player
    Made some changes to how mobs are created and made them follow the player's position.

--- js/Gameplay.js
+++ js/Gameplay.js
@@ -9,6 +9,7 @@ Gameplay.preload = function() {
 var map;
 var layer;
 var playerSprite;
+var mobs = [];
 
 Gameplay.create = function() {
 	map = game.add.tilemap('test');
@@ -23,15 +24,21 @@ Gameplay.create = function() {
 	// M to spawn a mob
 	spawnMobKey = game.input.keyboard.addKey(Phaser.Keyboard.M);
 	spawnMobKey.onDown.add(() => {
-		new MobBadGuy(this);
+	//	new MobBadGuy(this);
+		mobs.push(new Mob(this));	
 	});
 
 	playerSprite = game.add.sprite(game.camera.x + game.camera.width / 2, game.camera.y + game.camera.height / 2, 'player');
-	game.debug.body(playerSprite);
+	playerSprite.anchor.setTo(0.5, 0.5);
+	game.physics.arcade.enable(playerSprite);
 	playerSprite.scale.setTo(2, 2);
+	playerSprite.body.immovable = true;
 }
 
 Gameplay.update = function() {
+	for (var i = 0; i < mobs.length; i++) {
+		mobs[i].update();
+	}
 	if (upKey.isDown) {
 		if (playerSprite.y <= game.camera.y + game.camera.height / 2) {
 			game.camera.y-=5;
@@ -57,4 +64,8 @@ Gameplay.update = function() {
 		}
 		playerSprite.x+=5;
 	}
+}
+
+Gameplay.getPlayer = function() {
+	return playerSprite;
 }
