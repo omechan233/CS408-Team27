60abff918b755e6cb46e8ef620be5d115a5cc3e7
devonlee111 <devonlee111@gmail.com>
2018-02-10 14:46:55 -0500

    Fixed Start Button Bug
    Fixed a bug that in the Sprint1 branch that caused the start button to not change textures when you hover over it. Fixed it by localising all functions in the menu.

--- js/Menu.js
+++ js/Menu.js
@@ -17,36 +17,36 @@ Menu.create = function() {
 	game.world.height = game.camera.height;
 
 	game.stage.backgroundColor = '#2a93c7';
-	startBtn = game.add.button(0, 0, 'start', startGame, this);
+	startBtn = game.add.button(0, 0, 'start', this.startGame, this);
 	startBtn.scale.setTo(1.2, 1.2);
 	startBtn.x = game.world.centerX - (startBtn.width / 2);
 	startBtn.y = game.world.centerY - (startBtn.height / 2) + 40;
-	startBtn.onInputOver.add(startOver, this);
-	startBtn.onInputOut.add(startOut, this);
+	startBtn.onInputOver.add(this.startOver, this);
+	startBtn.onInputOut.add(this.startOut, this);
 
-	scoreBtn = game.add.button(0, 0, 'score', viewScore, this);
+	scoreBtn = game.add.button(0, 0, 'score', this.viewScore, this);
 	scoreBtn.scale.setTo(1.2, 1.2);
 	scoreBtn.x = game.world.centerX - (scoreBtn.width / 2);
 	scoreBtn.y = game.world.centerY - (scoreBtn.height / 2) + 130;
-	scoreBtn.onInputOver.add(scoreOver, this);
-	scoreBtn.onInputOut.add(scoreOut, this);
+	scoreBtn.onInputOver.add(this.scoreOver, this);
+	scoreBtn.onInputOut.add(this.scoreOut, this);
 
-	accountBtn = game.add.button(10, 10, 'account', viewProfile, this);
+	accountBtn = game.add.button(10, 10, 'account', this.viewProfile, this);
 	accountBtn.scale.setTo(1.2, 1.2);
-	accountBtn.onInputOver.add(accountOver, this);
-	accountBtn.onInputOut.add(accountOut, this);
+	accountBtn.onInputOver.add(this.accountOver, this);
+	accountBtn.onInputOut.add(this.accountOut, this);
 
-	difficultyBtn = game.add.button(startBtn.x + 15, startBtn.y + 45, 'difficulty', changeDifficulty, this);
+	difficultyBtn = game.add.button(startBtn.x + 15, startBtn.y + 45, 'difficulty', this.changeDifficulty, this);
 	difficultyBtn.scale.setTo(.8, .8);
 	var style = { font: "Lucida Console", fontSize: "18px", wordWrap: false, align: "center", fontWeight: "bold" };
 	difficultyText = game.add.text(difficultyBtn.x + 150, difficultyBtn.y + 8, "Difficulty: easy", style);
 }
 
-function showDifficulty() {
+Menu.showDifficulty = function() {
 	difficultyText.setText("Difficulty: " + difficulty);
 }
 
-function changeDifficulty() {
+Menu.changeDifficulty = function() {
 	switch(difficulty) {
 		case "easy":
 			difficulty = "medium";
@@ -60,43 +60,43 @@ function changeDifficulty() {
 			difficulty = "easy";
 			break;
 	}
-	showDifficulty();
+	this.showDifficulty();
 }
 
-function startGame() {
+Menu.startGame = function() {
 	game.state.start('Gameplay');
 }
 
-function viewScore() {
+Menu.viewScore = function() {
 	game.stage.backgroundColor = '#00ff00';
 	game.state.start('HighScores');
 }
 
-function viewProfile() {
+Menu.viewProfile = function() {
 	game.stage.backgroundColor = '#0000ff';
 	game.state.start('Account');
 }
 
-function startOver() {
+Menu.startOver = function() {
 	startBtn.loadTexture('startActive');
 }
 
-function startOut() {
+Menu.startOut = function() {
 	startBtn.loadTexture('start');
 }
 
-function scoreOver() {
+Menu.scoreOver = function() {
 	scoreBtn.loadTexture('scoreActive');
 }
 
-function scoreOut() {
+Menu.scoreOut = function() {
 	scoreBtn.loadTexture('score');
 }
 
-function accountOver() {
+Menu.accountOver = function() {
 	accountBtn.loadTexture('accountActive');
 }
 
-function accountOut() {
+Menu.accountOut = function() {
 	accountBtn.loadTexture('account');
 }
