6361dfc5645aa16270f3826e0aec89bad02b374c
omechan233 <31775045+omechan233@users.noreply.github.com>
2018-02-10 10:30:29 +0900

    Add files via upload

---
+++ assets/base.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="base" tilewidth="32" tileheight="32" tilecount="400" columns="8">
+ <image source="E:/360Downloads/RM/梦の彼方/Graphics/Tilesets/base.png" width="256" height="1600"/>
+</tileset>
