cb4d83705c847443b16434131db5a4c75967155c
omechan233 <31775045+omechan233@users.noreply.github.com>
2018-02-15 06:21:17 +0900

    Add files via upload

---
+++ assets/lab.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="lab" tilewidth="32" tileheight="32" tilecount="3248" columns="8">
+ <image source="E:/360Downloads/RM/dream far/Graphics/Tilesets/研究设施.png" width="256" height="13000"/>
+</tileset>
